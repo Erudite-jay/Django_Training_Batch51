50c52286dadd226bc600ede9f558fe7a2b2b33bb
Jay <jaydwivedi.programmer@gmail.com>
2025-01-16 16:02:42 -0500

    Day 1 Session 2

---
+++ mysite/Auth_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   path("print-hello/",views.hello_world),
+   path("home/",views.home_page),
+]
--- mysite/Auth_app/views.py
+++ mysite/Auth_app/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def hello_world(request):
+   return HttpResponse("Hello, world! i am backend , first view")
+
+def home_page(request):
+   return render(request, 'Auth_app/index.html')
