0792e0bbadc817950cef8d10a9c244ab5f9a57cf
Jay <jaydwivedi.programmer@gmail.com>
2025-01-22 15:35:48 -0500

    Day 4 Session 2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'Auth_app',
     'Form_app',
     'Session_app',
+    'JWT_app',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,10 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
